dc190d90379f5216a76e057b7151eed70676f8d2
iranimoroni <iranimoroni@gmail.com>
2020-09-29 15:05:52 -0300

    first commit

---
+++ gitStart.bat
@@ -0,0 +1,7 @@
+echo "# gincana" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/iranimoroni/gincana.git
+git push -u origin master
