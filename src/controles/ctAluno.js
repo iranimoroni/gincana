44bd121855fcb43c70c311810dc68be388e11faa
iranimoroni <iranimoroni@gmail.com>
2020-11-19 14:01:34 -0300

    virgula

--- src/controles/ctAluno.js
+++ src/controles/ctAluno.js
@@ -127,7 +127,7 @@ module.exports = {
             console.log(error);
             return res.json("Erro ao gravar aluno");
         };
-    }
+    },
         //======================================================================
     async applistaAlunos(req, res) {
         try {
